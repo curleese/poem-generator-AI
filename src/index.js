304dcaea1389554403de8b0a2a5537f0d2f4cdcd
curleese <bebecurlee@gmail.com>
2025-12-15 19:57:33 -0500

    added js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("hello world");
+function generatePoem(event){
+    event.preventDefault();
+
+    new Typewriter("#poem", {
+        strings: "La tombe dit à la rose",
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+    });
+}
+
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener('submit', generatePoem);
